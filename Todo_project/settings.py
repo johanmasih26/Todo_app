09894fb4fd05a496eb9000eb1e2ceb63f1479267
johan <harryjohn026@gmail.com>
2021-08-14 16:09:40 +0530

    static collected

--- Todo_project/settings.py
+++ Todo_project/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static/')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 LOGIN_URL = 'login' 
 # Default primary key field type
